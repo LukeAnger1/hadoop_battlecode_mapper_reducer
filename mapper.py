5f977e7ae26d032249a434efafa906e27c472441
Luke <anger.luke2004@gmail.com>
2024-01-09 10:32:47 -0500

    moved the file making code to reducer as it needs it and not mapper

--- mapper.py
+++ mapper.py
@@ -5,34 +5,13 @@
 # BotName | Map1, Map2, Map3, Map4, ... | var1 -> range() | var2 -> range() | var3 -> range()
 # Then we have multiple lines of this for multiple bots
 
+# Will output
+# BotName1, var1 -> value1, var2 -> value2, ... | BotName2, var1 -> value1, var2 -> value2, ... | Map1, Map2, Map3, Map4, Map5, ...
+
 import sys
 import re
 import itertools
 import random
-
-# TODO: The current is just for testing and should be switched later
-bot_source_file_path = "/mnt/c/Users/anger/OneDrive/Desktop/bc/test_file.txt"
-path_to_bot_src_folder = "/mnt/c/Users/anger/OneDrive/Desktop/bc" # This is where it puts the modified content
-start_char, end_char = '<?', '?>' # These are the characters the code looks for for replacing words
-
-# Function to replace words
-def replace_words_func(text, original, replace):\
-    # This is to make sure original and replace is some sort of list
-    assert not isinstance(original, str) and not isinstance(replace, str)
-    # This will take text and two lists, and replace the original with the replacement
-    for old_word, new_word in zip(original, replace):
-        text = text.replace(old_word, str(new_word))
-    return text
-
-# This function will get src file and make the one replaced, THIS ADDS THE CHARACTERS
-def make_bot(input_file_path, output_file_path, original_words, replace_words):
-    # this adds the characters
-    original_words = [start_char + original_word + end_char for original_word in original_words]
-    with open(input_file_path, 'r') as file:
-        file_content = file.read()
-    modified_content = replace_words_func(file_content, original_words, replace_words)
-    with open(output_file_path, 'w') as file:
-            file.write(modified_content)
 
 # This will get a list of every possible combination
 def get_all_combinations(replace_range):
@@ -43,9 +22,6 @@ def get_rand_combinations(replace_range, number_combination):
     return random.sample(get_all_combinations(replace_range), number_combination)
 
 if __name__ == '__main__':
-    # testing for bot file writing start
-    # make_bot(bot_source_file_path, path_to_bot_src_folder + '/output.txt', ['var1', 'var2'], ['LLLLLL', 'Take the L'])
-    # testing for bot file writing end
 
     for line in sys.stdin:
         line = re.sub(r'\W+', ' ', line.strip())
--- reducer.py
+++ reducer.py
@@ -8,33 +8,62 @@ current_word = None
 current_count = 0
 word = None
 
-# input comes from STDIN
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+# TODO: The current is just for testing and should be switched later
+bot_source_file_path = "/mnt/c/Users/anger/OneDrive/Desktop/bc/test_file.txt"
+path_to_bot_src_folder = "/mnt/c/Users/anger/OneDrive/Desktop/bc" # This is where it puts the modified content
+start_char, end_char = '<?', '?>' # These are the characters the code looks for for replacing words
 
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+# Function to replace words
+def replace_words_func(text, original, replace):\
+    # This is to make sure original and replace is some sort of list
+    assert not isinstance(original, str) and not isinstance(replace, str)
+    # This will take text and two lists, and replace the original with the replacement
+    for old_word, new_word in zip(original, replace):
+        text = text.replace(old_word, str(new_word))
+    return text
 
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
+# This function will get src file and make the one replaced, THIS ADDS THE CHARACTERS
+def make_bot(input_file_path, output_file_path, original_words, replace_words):
+    # this adds the characters
+    original_words = [start_char + original_word + end_char for original_word in original_words]
+    with open(input_file_path, 'r') as file:
+        file_content = file.read()
+    modified_content = replace_words_func(file_content, original_words, replace_words)
+    with open(output_file_path, 'w') as file:
+            file.write(modified_content)
 
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
+if __name__ == '__main__':
+    # testing for bot file writing start
+    # make_bot(bot_source_file_path, path_to_bot_src_folder + '/output.txt', ['var1', 'var2'], ['LLLLLL', 'Take the L'])
+    # testing for bot file writing end
+
+    # input comes from STDIN
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+
+        # parse the input we got from mapper.py
+        word, count = line.split('\t', 1)
+
+        # convert count (currently a string) to int
+        try:
+            count = int(count)
+        except ValueError:
+            # count was not a number, so silently
+            # ignore/discard this line
+            continue
+
+        # this IF-switch only works because Hadoop sorts map output
+        # by key (here: word) before it is passed to the reducer
+        if current_word == word:
+            current_count += count
+        else:
+            if current_word:
+                # write result to STDOUT
+                print(f'{current_word}\t{current_count}')
+            current_count = count
+            current_word = word
+
+    # do not forget to output the last word if needed!
     if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print(f'{current_word}\t{current_count}')
-        current_count = count
-        current_word = word
-
-# do not forget to output the last word if needed!
-if current_word == word:
-    print(f'{current_word}\t{current_count}')
+        print(f'{current_word}\t{current_count}')
