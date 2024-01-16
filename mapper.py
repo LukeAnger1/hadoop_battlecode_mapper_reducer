7d73d2de4948d9a8f18aef3625b5442519329cbb
Luke <anger.luke2004@gmail.com>
2024-01-16 17:02:26 -0500

    changed file paths back

--- mapper.py
+++ mapper.py
@@ -47,10 +47,10 @@ def get_rand_combinations_one_bot(replace_range, number_combination):
     return random.sample(get_all_combinations_one_bot(replace_range), number_combination)
 
 # TODO: The current is just for testing and should be switched later
-folder_with_gradlew = "/home/hduser/yaro"
-bot_source_file_folder_with_dummy_variables = "src" # this is the folder to look for the bots (any b>
+folder_with_gradlew = "/home/langer1/bc/yaro"
+bot_source_file_folder_with_dummy_variables = "/home/langer1/bc/mr/src" # this is the folder to look for the bots (any bot name should have a file in this folder) that need to variables to be replaced
 # IMPORTANT make sure the below file is right it will delete all bots!!!!
-bot_source_file_folder = "/home/hduser/yaro/src" # This is where it puts the modified content, this >
+bot_source_file_folder = "/home/langer1/bc/yaro/src" # This is where it puts the modified content, this is what the game will run
 
 # Function to replace words
 def replace_words_func(text, original, replace):\
